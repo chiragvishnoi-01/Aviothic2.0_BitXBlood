35c622a4511b0b9265af50a67fbd910687b0aa40
chirag vishnoi <chiragvishnoi96@gmail.com>
2025-10-14 10:43:34 +0530

    Configure for GitHub Pages deployment

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: '/Aviothic2.0_BitXBlood/',
   server: {
     port: 5173,
     strictPort: false,
